0ede44c03b832dc3c236c6970d8a8f37cc81eb84
xlpaladinolx <xlpaladinolx@gmail.com>
2022-03-15 23:17:44 -0300

    Enviando via Bash Git

--- siteSEW/settings.py
+++ siteSEW/settings.py
@@ -17,6 +17,7 @@ import rest_framework
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Adicionando novas coisas
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
